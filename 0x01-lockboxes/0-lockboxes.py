c96df3e66f21f6b020aa3384da2ccfe018c27689
sayv <98850746+sayv-xo@users.noreply.github.com>
2024-10-21 07:55:54 +0000

    lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,16 @@
+#!usr/bin/python3
+"""lockboxes"""
+
+
+def canUnlockAll(boxes):
+    """checks if all the boxes can be opened"""
+    if not boxes:
+        return False
+
+    keys = [0]
+    for key in keys:
+        for new_key in boxes[key]:
+            if new_key not in keys and new_key < len(boxes):
+                keys.append(new_key)
+
+    return len(keys) == len(boxes)
